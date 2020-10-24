e7e7ef86a63ccd8035328745965a35b0d1cc6156
Kristof S <kristof.attila.private@gmail.com>
2020-10-24 15:56:29 +0200

    wrapper functional

--- src.cython/cyarray/include/mstr.pxd
+++
@@ -1,6 +0,0 @@
-
-# mstr, a struct for holding externally owned string data
-
-cdef struct mstr:
-    char* string
-    size_t size
--- src.cython/cythonize.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules = cythonize("cythonMeshReduction2.pyx", language_level = "3",annotate=True),
-    include_dirs=[numpy.get_include()]
-)
