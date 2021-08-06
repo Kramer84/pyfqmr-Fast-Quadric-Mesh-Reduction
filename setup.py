61e1d5e2a10a7748c534a4d672c67b1e2abfa4c4
kramer84 <kristof.attila.private@gmail.com>
2021-08-06 22:42:37 +0200

    some cleanups

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-#from distutils.core import setup, Extension
-from setuptools import find_packages, setup
-from Cython.Build import cythonize
+from setuptools import setup
+from setuptools.extension import Extension
 import numpy
+
+#from  https://github.com/AshleySetter/HowToPackageCythonAndCppFuncs
+
 
 # load the version from the file
 with open("VERSION", 'r') as fic:
@@ -10,16 +12,29 @@ with open("VERSION", 'r') as fic:
 with open('README.md') as f:
     readme = f.read()
 
+extensions = [
+    Extension(
+    name="pyFMR.SumArray", # name/path of generated .so file
+    sources=["cythonSumPackage/SumArrayCython/SumArray.c"], # cython generated c file
+    include_dirs = [numpy.get_include()]), # gives access to numpy funcs inside cython code 
+    Extension(
+    name="cythonSumPackage.SumArrayCythonCpp", # name/path of generated .so file
+    sources=["cythonSumPackage/SumArrayC/SumArrayCythonCpp.cpp"], # cython generated cpp file
+    include_dirs = [numpy.get_include()], # gives access to numpy funcs inside cython code 
+    language="c++",), # tells python that the language of the extension is c++
+]
+
 setup(
-    ext_modules = cythonize(["src/simplify_wrapper.pyx", "src/__init__.pyx"], language_level = "3",annotate=True),
-    include_dirs=[numpy.get_include()],
     name = "pyFMR",
+    cmdclass=cmdclass,
+    ext_modules =  ext_modules, #cythonize(["src/simplify_wrapper.pyx", "src/__init__.pyx"], language_level = "3",annotate=True),
+    include_dirs=[numpy.get_include()],
     version=version,
     description = "cython wrapper around C++ library for fast triangular mesh reduction",
     author="kramer84",
     url = "https://github.com/Kramer84/pyFMR-Fast-quadric-Mesh-Reduction-",
     license='MIT',
-    packages= find_packages(), #['pyFMR'],
+    packages= ['pyFMR'],
     #package_dir={
     #'pyFMR':'src'
     #},
