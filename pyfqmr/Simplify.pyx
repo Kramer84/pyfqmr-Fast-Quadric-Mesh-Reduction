b463ec15cc52ae3f5dbb5f33c110e00623489e9f
kramer84 <kristof.attila.private@gmail.com>
2021-08-09 16:18:26 +0200

    clearing vectors to avoid out of bound buffer access error

--- pyfqmr/Simplify.pyx
+++ pyfqmr/Simplify.pyx
@@ -79,10 +79,10 @@ cdef class Simplify :
         self.vertices_cpp.clear()
         self.normals_cpp.clear()
         # Here we will need some checks, just to make sure the right objets are passed
-        self.faces_mv      = faces.astype(dtype="int32", subok=False, copy=False)
-        self.vertices_mv   = vertices.astype(dtype="float64", subok=False, copy=False)
+        self.faces_mv = faces.astype(dtype="int32", subok=False, copy=False)
+        self.vertices_mv = vertices.astype(dtype="float64", subok=False, copy=False)
         self.triangles_cpp = setFacesNogil(self.faces_mv, self.triangles_cpp)
-        self.vertices_cpp  = setVerticesNogil(self.vertices_mv, self.vertices_cpp)
+        self.vertices_cpp = setVerticesNogil(self.vertices_mv, self.vertices_cpp)
         setMeshFromExt(self.vertices_cpp, self.triangles_cpp)
 
     cpdef void simplify_mesh(self, int target_count = 100, int update_rate = 5, 
