d55831dccd9514b58c469f8f6834baa7d8b66e9a
Kramer84 <kristof.attila.private@gmail.com>
2024-10-20 15:59:35 +0200

    Added lossless tests, passed version to 0.3.0

--- tests/test_simplify.py
+++ tests/test_simplify.py
@@ -35,3 +35,28 @@ def test_empty():
     assert len(vertices) == 0
     assert len(faces) == 0
     assert len(normals) == 0
+
+def test_example_lossless():
+    import trimesh as tr
+    bunny = tr.load_mesh(EXAMPLES_DIR / 'Stanford_Bunny_sample.stl')
+    simp = pyfqmr.Simplify()
+    simp.setMesh(bunny.vertices, bunny.faces)
+    simp.simplify_mesh_lossless()
+    vertices, faces, normals = simp.getMesh()
+
+    assert len(faces) / len(bunny.faces) == pytest.approx(.5334, rel=.05)
+    simplified = tr.Trimesh(vertices, faces, normals)
+    assert simplified.area == pytest.approx(simplified.area, rel=.05)
+
+def test_empty_lossless():
+    verts = np.zeros((0,3), dtype=np.float32)
+    faces = np.zeros((0,3), dtype=np.float32)
+
+    simp = pyfqmr.Simplify()
+    simp.setMesh(verts, faces)
+    simp.simplify_mesh_lossless()
+    vertices, faces, normals = simp.getMesh()
+
+    assert len(vertices) == 0
+    assert len(faces) == 0
+    assert len(normals) == 0
